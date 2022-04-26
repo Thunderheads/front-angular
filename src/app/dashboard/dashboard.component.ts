50f92c3a10d20a34754a1f4fc5864ae2a19bea78
Thunderheads <amoknajim@gmail.com>
2022-04-26 15:46:37 +0200

    " ajout des filtres sur la page home "

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TableData} from "../../service/api/table.data";
 import {IApplication} from "../../modeleInterface/IApplication";
+import {Router} from "@angular/router";
+import {DatasData} from "../../service/api/datas.data";
+import {ApplicationDatas} from "../../service/api/application.datas";
 
 @Component({
   selector: 'app-dashboard',
@@ -9,12 +12,52 @@ import {IApplication} from "../../modeleInterface/IApplication";
 })
 export class DashboardComponent implements OnInit {
 
+  public datas: IApplication[] = [];
+  public id_app : number | undefined;
+  public ordre : string | undefined;
 
 
+
+  constructor(private appData: ApplicationDatas) {
+
+  }
 
 
   ngOnInit(): void {
 
+    this.appData.get('http://localhost/test/public/api/application/name').subscribe(
+      data => {
+        for (let element of data) {
+          this.datas.push(element);
+        }
+        console.log(this.datas)
+      }
+      )
+
+
   }
+
+
+  /**
+   * Fonction en charge de renvoyer l'id de l'application
+   * @param value
+   */
+  selectedApp(value: any) {
+    this.id_app = value
+  }
+
+  /**
+   * Fonction en charge de renvoyer l'ordre de tri
+   * @param value
+   */
+  selectedNote(value: any) {
+    this.ordre = value
+  }
+}
+
+
+export interface IFilter{
+ id_app : number | undefined;
+ ordre : string | undefined;
 
 }
--- src/app/dashboard/table/table.component.ts
+++ src/app/dashboard/table/table.component.ts
@@ -1,10 +1,12 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, SimpleChanges, ViewChild} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import {IApplication} from "../../../modeleInterface/IApplication";
 import {TableData} from "../../../service/api/table.data";
 import {IHomePage} from "../../../modeleInterface/IHomePage";
 import {Router} from "@angular/router";
+import {IData} from "../../../modeleInterface/IData";
+import {isEmpty} from "rxjs";
 
 /**
  * @title Table with pagination
@@ -16,6 +18,8 @@ import {Router} from "@angular/router";
 })
 export class TableComponent implements  OnInit {
 
+  @Input() donnes: number | undefined;
+  @Input() donnes2: string | undefined;
   displayedColumns: string[] = ['Nom', 'Note', 'Avis', 'OS', 'Detail'];
   datas : IHomePage[] = [];
   dataSource : any;
@@ -44,6 +48,38 @@ export class TableComponent implements  OnInit {
 
   redirection(id : number) {
     this.router.navigateByUrl('/afficher/'+ id);
+  }
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+    let url = 'http://localhost/test/public/api/application/param'
+    if(this.donnes !== undefined && this.donnes2 !== undefined){
+      url = url  +'?id='+ this.donnes + '&ordre=' +this.donnes2;
+    } else {
+      if(this.donnes !== undefined){
+        url = url  +'?id='+ this.donnes;
+      }
+
+      if(this.donnes2 !== undefined){
+        url = url +'?ordre=' +this.donnes2;
+      }
+    }
+
+
+      this.appData.get(url).
+      subscribe(
+        data => {
+          this.datas = [];
+          for (let element of data){
+            this.datas.push(element);
+          }
+          this.dataSource = new MatTableDataSource<IHomePage>(this.datas);
+          this.dataSource.paginator = this.paginator;
+        }
+      )
+
+
   }
 }
 

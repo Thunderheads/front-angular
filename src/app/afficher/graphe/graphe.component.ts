aa0228b3b605d88eaa248fd6f5c2e120e1928d72
Thunderheads <amoknajim@gmail.com>
2022-04-29 17:45:01 +0200

    "Factorisation de la fonction change dans classe graphe"

--- src/app/afficher/graphe/graphe.component.ts
+++ src/app/afficher/graphe/graphe.component.ts
@@ -26,6 +26,10 @@ export class GrapheComponent implements OnInit, OnChanges  {
 
   constructor(private appData: ApplicationDatas, private route: ActivatedRoute) {
   }
+  //activé quand changement dans le composant parent
+  ngOnChanges(changes: SimpleChanges): void {
+    this.change('week')
+  }
 
   ngOnInit(): void {
     this.currentIDApp = this.route.snapshot.paramMap.get('id')!;
@@ -115,7 +119,7 @@ export class GrapheComponent implements OnInit, OnChanges  {
    * @param lstDatas
    * @return string[]
    */
-  public listeDate(lstDatas: IData[]): string[] {
+  private listeDate(lstDatas: IData[]): string[] {
     let lstDate: string[] = [];
 
     for (let data of lstDatas) {
@@ -131,7 +135,7 @@ export class GrapheComponent implements OnInit, OnChanges  {
    * @param lstRating
    * @return number[]
    */
-  public listeRating(lstDatas: IData[]): number[] {
+  private listeRating(lstDatas: IData[]): number[] {
     let lstRating: number[] = [];
 
     for (let data of lstDatas) {
@@ -145,7 +149,7 @@ export class GrapheComponent implements OnInit, OnChanges  {
    * @param lstVote
    * @return number[]
    */
-  public listeVote(lstDatas: IData[]): number[] {
+  private listeVote(lstDatas: IData[]): number[] {
     let lstVote: number[] = [];
 
     for (let data of lstDatas) {
@@ -154,58 +158,16 @@ export class GrapheComponent implements OnInit, OnChanges  {
     return lstVote;
   }
 
+  /**
+   * Fonction en charge de mettre a jour la liste des données en fonction de la variable ( week, month ou threeMonth)
+   * @param value
+   */
+  public change(value : string) {
 
-  /// A FACTORISER !!!!!!
-  changeToWeek() {
-    let chartStatus = Chart.getChart("my_first_chart");
-    if (chartStatus != undefined) {
-      chartStatus.destroy();
-    }
-    let chartStatus2 = Chart.getChart("my_second_chart");
-    if (chartStatus2 != undefined) {
-      chartStatus2.destroy();
-    }
-
-    this.chartRating = document.getElementById('my_first_chart');
-    this.chartVote = document.getElementById('my_second_chart');
-
-    this.dataStorage = [];
-    for (let myData of this.donnes!) {
-      //si la date du jour - date collect est en dessous de 7 jours on stocke les données qu'on affichera plus tard
-      if (Math.floor((new Date(Date.now()).getTime() - new Date(myData.dateCollect).getTime()) / 1000 / 60 / 60 / 24) <= 7) {
-        // on ajoute dans une liste les données concernant ces jours au format IData
-        this.dataStorage?.push(myData);
-      }
-    }
-    this.loadChartVote();
-    this.loadChartRating();
-  }
-
-  changeToMonth() {
-    let chartStatus = Chart.getChart("my_first_chart");
-    if (chartStatus != undefined) {
-      chartStatus.destroy();
-    }
-    let chartStatus2 = Chart.getChart("my_second_chart");
-    if (chartStatus2 != undefined) {
-      chartStatus2.destroy();
-    }
-
-    this.chartRating = document.getElementById('my_first_chart');
-    this.chartVote = document.getElementById('my_second_chart');
-    this.dataStorage = [];
-    for (let myData of this.donnes!) {
-      //si la date du jour - date collect est en dessous de 7 jours on stocke les données qu'on affichera plus tard
-      if (Math.floor((new Date(Date.now()).getTime() - new Date(myData.dateCollect).getTime()) / 1000 / 60 / 60 / 24) <= 31) {
-        // on ajoute dans une liste les données concernant ces jours au format IData
-        this.dataStorage?.push(myData);
-      }
-    }
-    this.loadChartVote();
-    this.loadChartRating();
-  }
-
-  changeToThreeMonth() {
+    let days = {
+      "week" : 7,
+      "month" : 30,
+      "threeMonth" : 90}
 
     let chartStatus = Chart.getChart("my_first_chart");
     if (chartStatus != undefined) {
@@ -222,20 +184,35 @@ export class GrapheComponent implements OnInit, OnChanges  {
 
     this.dataStorage = [];
     for (let myData of this.donnes!) {
-      //si la date du jour - date collect est en dessous de 7 jours on stocke les données qu'on affichera plus tard
-      if (Math.floor((new Date(Date.now()).getTime() - new Date(myData.dateCollect).getTime()) / 1000 / 60 / 60 / 24) <= 90) {
-        // on ajoute dans une liste les données concernant ces jours au format IData
-        this.dataStorage?.push(myData);
+      if (value == "week"){
+        //si la date du jour - date collect est en dessous de 7 jours on stocke les données qu'on affichera plus tard
+        if (Math.floor((new Date(Date.now()).getTime() - new Date(myData.dateCollect).getTime()) / 1000 / 60 / 60 / 24) <= days.week ) {
+          // on ajoute dans une liste les données concernant ces jours au format IData
+          this.dataStorage?.push(myData);
+        }
       }
+      if (value == "month"){
+        //si la date du jour - date collect est en dessous de 7 jours on stocke les données qu'on affichera plus tard
+        if (Math.floor((new Date(Date.now()).getTime() - new Date(myData.dateCollect).getTime()) / 1000 / 60 / 60 / 24) <= days.month ) {
+          // on ajoute dans une liste les données concernant ces jours au format IData
+          this.dataStorage?.push(myData);
+        }
+      }
+
+      if (value == "threeMonth"){
+        //si la date du jour - date collect est en dessous de 7 jours on stocke les données qu'on affichera plus tard
+        if (Math.floor((new Date(Date.now()).getTime() - new Date(myData.dateCollect).getTime()) / 1000 / 60 / 60 / 24) <= days.threeMonth ) {
+          // on ajoute dans une liste les données concernant ces jours au format IData
+          this.dataStorage?.push(myData);
+        }
+      }
+
     }
     this.loadChartVote();
     this.loadChartRating();
   }
 
-  //activé quand changement dans le coposant parent
-  ngOnChanges(changes: SimpleChanges): void {
-    this.changeToWeek()
-  }
+
 
 
 }
